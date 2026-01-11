d077702e346314daea411cb770be790765d95501
baumleaf <iamtreeleaf64@gmail.com>
2026-01-11 21:43:26 +0700

    add fps rule

--- main.py
+++ main.py
@@ -11,13 +11,18 @@ def main():
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        # Limit the framerate to 60FPS
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
