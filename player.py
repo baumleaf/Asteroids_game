b7c9e400b1de237d512c83a5200d942446ebf729
baumleaf <iamtreeleaf64@gmail.com>
2026-01-18 23:10:39 +0700

    add bullet rate limit

--- player.py
+++ player.py
@@ -4,6 +4,7 @@ from circleshape import CircleShape
 from constants import (
     LINE_WIDTH,
     PLAYER_RADIUS,
+    PLAYER_SHOOT_COOLDOWN_SECONDS,
     PLAYER_SHOOT_SPEED,
     PLAYER_SPEED,
     PLAYER_TURN_SPEED,
@@ -17,6 +18,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0
 
     # in the Player class
 
@@ -42,6 +44,7 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.shoot_timer -= dt
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -55,6 +58,10 @@ class Player(CircleShape):
             self.shoot()
 
     def shoot(self):
+        if self.shoot_timer > 0:
+            return
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN_SECONDS
+
         new_shot = Shot(self.position.x, self.position.y)
         velocity_vector = pygame.Vector2(0, 1)
         rotated_velocity = velocity_vector.rotate(self.rotation)
